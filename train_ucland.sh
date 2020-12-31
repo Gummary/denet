7d324447a29d0f2341f4e228268aefbe9d804bcb
Gummary <sdythp@gmail.com>
2020-12-31 14:24:23 +0800

    update train script

--- train_ucland.sh
+++ train_ucland.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+if [ $# != 2 ] ; then
+  echo "USAGE: $0 NET EXPName"
+  echo " e.g.: $0 RCAN exp1"
+  exit 1;
+fi
+
+
+
 python main.py \
-    --model RCAN \
+    --model $1 \
     --dataset UCLand \
     --scale 4 \
     --batch_size 16 \
     --max_steps 100000 \
-    --dataset_root dataset/UCMerced_LandUse/
+    --dataset_root dataset/UCMerced_LandUse/ \
+    --save_root ./output/$1/$2 \
+    --ckpt_root ./pt/$1/$2
